35add0b08f494230f50fd292f4d2cd1a1e43a135
Maurice-Jah <mauricejahwizwid@gmail.com>
2023-05-16 14:20:49 +0100

    Add leaflet js with the css

--- script.js
+++ script.js
@@ -18,9 +18,21 @@ if (navigator.geolocation)
       const { latitude } = position.coords;
       const { longitude } = position.coords;
 
-      console.log(latitude, longitude);
-
       console.log(`https://www.google.ng/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('could not get your position');
